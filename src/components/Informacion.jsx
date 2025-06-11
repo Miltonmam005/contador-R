32651786fa3cd2bf4193faa33c89458abc7c0082
Milton Mamani <miltin.mamani010@gmail.com>
2025-06-10 23:24:27 -0300

    agregamos otra sintaxis para los componentes

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,13 +1,13 @@
-const Informacion = (props) => {
+const Informacion = ({ anio, comisionProps }) => {
   return (
     <section>
       <h2> primer proyecto con react </h2>
       <p>
         React es una biblioteca de JavaScript desarollada por facebook para
         construir interfaces de usuario. <br /> Se destaca por su enfoque en la
-        creacion de aplicaciones web de una sola pagina SPA y su capacidad
-        para manejar la interfaz de menara eficiente mediante el uso de un <b> DOM
-        virtual</b>
+        creacion de aplicaciones web de una sola pagina SPA y su capacidad para
+        manejar la interfaz de menara eficiente mediante el uso de un{" "}
+        <b> DOM virtual</b>
       </p>
       <p>
         React permite a los desarrolladores construir interfaces dinamicas y
@@ -15,7 +15,10 @@ const Informacion = (props) => {
         mantenimiento de aplicaciones a gran escala.
       </p>
       <article className=" bg-primary-subtle p-3 rounded-3">
-        <p> Practica de contador de la <strong>comision {props.comisionProps} </strong> </p>
+        <p>
+          Practica de contador de la <strong>comision {comisionProps}</strong>-
+          {anio}
+        </p>
       </article>
     </section>
   );
