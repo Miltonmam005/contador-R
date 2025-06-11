72359c47b5b16f897c479effd85278af0731ac78
Milton Mamani <miltin.mamani010@gmail.com>
2025-06-11 00:36:16 -0300

    agrege los botones de borrar y sumar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 const Contador = () => {
-    return (
-        <section className="text-center mt-5">
-            <h2> Contador </h2>
-            <h3> N°: 1</h3>
-            <button className="btn btn-primary me-3"> + </button>
-            <button className="btn btn-danger"> - </button>
-        </section>
-    );
+  // crear un estado
+  const [numero, setNumero] = useState(1)
+  const sumar = ()=>{
+    setNumero(numero + 1)
+    console.log(numero);
+  }
+
+  const restar =()=>{
+    setNumero(numero - 1)
+    console.log(numero)
+  }
+
+
+  return (
+    <section className="text-center mt-5">
+      <h2> Contador </h2>
+      <h3> N°: {numero}</h3>
+      <button className="btn btn-primary me-3" onClick={sumar}> +
+      </button>
+      <button className="btn btn-danger" onClick={restar}> - </button>
+    </section>
+  );
 };
 
 export default Contador;
